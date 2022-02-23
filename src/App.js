bbc1772f50f4291fc95d6aff74d8b060a67af643
Nahom Assefa <nzenebe@gmail.com>
2022-02-23 16:11:11 -0600

    20.3.4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from "react";
-import About from './components/About';
-import Nav from './components/Nav';
+import React, {useState} from "react";
+import About from "./components/About";
+import Nav from "./components/Nav";
+import Gallery from "./components/Gallery";
 
 function App() {
-  return (
-  <div>
-    <Nav />
-    <main>
+  const [categories] = useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
 
-        <About />
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  return (
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      <main>
+        <div>
+          <Gallery></Gallery>
+          <About></About>
+        </div>
       </main>
-  </div>
+    </div>
   );
 }
 
